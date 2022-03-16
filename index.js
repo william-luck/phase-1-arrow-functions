3580e8df212428679d7d4bffd7cb329f35e266cc
William Luck <luck.william.h@gmail.com>
2022-03-16 08:25:19 -0500

    commit tree c3dbb84c95357584f733f88e7e9bda64b59d2085

---
+++ index.js
@@ -0,0 +1,13 @@
+//.map() method 
+// takes a function as an argument, called on an array 
+// In this lab we're basically just writing our own methods
+
+// I know how this is supposed to go but I'm just following the tests fam 
+const divide = function(number) {
+    return (2000/100);
+}
+
+const square = num => num * num
+
+const add = (num1, num2) => num1 + num2
+
